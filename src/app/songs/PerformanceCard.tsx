381332031ebc5f4a4cccef04fa517a1b5f7fb9af
tylerrasor <trasor@gmail.com>
2024-02-14 10:03:22 -0800

    update song card and loading state to match

--- src/app/songs/PerformanceCard.tsx
+++ src/app/songs/PerformanceCard.tsx
@@ -15,26 +15,28 @@ export default function PerformanceCard({
 
   return (
     <li className="flex justify-between gap-x-6 py-5">
-      <div className="flex min-w-0 gap-x-4">
-        <div className="min-w-0 flex-auto">
-          <p className="text-xl font-semibold leading-6 text-gray-900 dark:text-gray-200">
-            <DateDisplay date={show.showDate} />
-          </p>
-          <p className="mt-1 truncate text-xs leading-5 text-gray-500 dark:text-gray-400">
-            {show.venue} -{' '}
-            {`${show.city}, ${show.state ? show.state : show.country}`}
+      <div className="border border-blue-300 shadow rounded-md p-4 max-w-xl5 w-full mx-auto">
+        <div className="flex min-w-0 gap-x-4">
+          <div className="min-w-0 flex-auto">
+            <p className="text-xl font-semibold leading-6 text-gray-900 dark:text-gray-200">
+              <DateDisplay date={show.showDate} />
+            </p>
+            <p className="mt-1 truncate text-xs leading-5 text-gray-500 dark:text-gray-400">
+              {show.venue} -{' '}
+              {`${show.city}, ${show.state ? show.state : show.country}`}
+            </p>
+          </div>
+        </div>
+        <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+          <p
+            className="text-5xl text-gray-900 dark:text-gray-200"
+            onClick={() => saveVote(performance.id)}
+          >
+            {/* placeholder idea - on hover, show upvote button */}
+            {/* for mobile, maybe have a ghosted upvote image because there's no hover?? */}
+            {5}
           </p>
         </div>
-      </div>
-      <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-        <p
-          className="text-5xl text-gray-900 dark:text-gray-200"
-          onClick={() => saveVote(performance.id)}
-        >
-          {/* placeholder idea - on hover, show upvote button */}
-          {/* for mobile, maybe have a ghosted upvote image because there's no hover?? */}
-          {5}
-        </p>
       </div>
     </li>
   )
--- src/app/songs/Song.tsx
+++ src/app/songs/Song.tsx
@@ -1,8 +1,5 @@
 'use client'
-import {
-  useGetAllPerformancesBySongQuery,
-  useSaveUserVoteMutation,
-} from '@/lib/services/musicService'
+import { useGetAllPerformancesBySongQuery } from '@/lib/services/musicService'
 import Loading from '../common/Loading'
 import PerformanceCard from './PerformanceCard'
 import SongHero from './SongHero'
@@ -16,8 +13,9 @@ export default function Song(): React.ReactNode {
       <SongHero />
       {isLoading ? (
         <>
-          {/** TODO: fix loading skeleton to look like actual song display */}
-          <Loading />
+          <div className="mx-auto max-w-4xl">
+            <Loading />
+          </div>
         </>
       ) : (
         <div className="mx-auto max-w-4xl">
